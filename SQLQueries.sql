019deb3c52c6e6446b96516f8ea1671aadd4a665
Samriddh Kulshrestha <codealone@users.noreply.github.com>
2021-04-27 09:55:01 +0530

    Update SQLQueries.sql

--- SQLQueries.sql
+++ SQLQueries.sql
@@ -7,3 +7,14 @@ CREATE TABLE Inventory (
     Price float,
     PRIMARY KEY (Id)
 );
+
+Stored Procedures:
+
+1. Create procedure DeleteItem    (     Id int     )      BEGIN            Delete from StudentReg where Id=Id;    END //
+
+2.Create procedure ListAll()    BEGIN            select * from inventory;    END //
+
+3.Create procedure AddNewItem    (         Name nvarchar (100),         Description nvarchar (1000),         Price float      )      BEGIN         Insert into inventory (Name, Description, Price)        values (Name, Description, Price) ;    END //
+
+4.Create procedure UpdateItem    (     Id int,       Name nvarchar (100),         Description nvarchar (1000),         Price float      )      BEGIN            Update inventory     set Name= Name,    Description=Description,                Price=Price            where Id=Id;    END //
+
